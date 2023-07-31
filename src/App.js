054a8d6943c2b72f9dfe3ba3af17d2dcc5ed5a89
Puttu Pavan Kumar <101029891+Pavan1471@users.noreply.github.com>
2023-07-31 21:27:26 +0530

    modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { useEffect } from "react";
+
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [users, setUsers] = useState([]);
+  const [temp, setTemp] = useState([]);
+  const [name, setName] = useState([]);
+
+
+  const LoadingSpinner = () => {
+ 
+
+    return (
+      <div>
+        {loading ? <div>Loading...</div> : null}
+      </div>
+    );
+  };
+
+  const changeEvent = (e) => {
+    setUsers(e.target.value);
+  };
+
+  const clickEvent = (e) => {
+    // const fetchData = () => {
+    console.log(users);
+    e.preventDefault();
+
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${users}&appid=4f13ad415eaff4542453cda4f44af349&units=metric`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setTemp(data.main.temp)
+        setName(data.name)
+      })
+      .catch(error => console.log(error))
+      
+      setLoading(false);
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <center>
+       
+      <div className="search-conatiner">
+        <input onChange={changeEvent} placeholder=" Search by Address"></input>
+        <button onClick={clickEvent}>Search</button>
+      </div>
+
+      <div>{temp}</div>
+      <div>{name}</div>
+      <LoadingSpinner />
+    </center>
+    
   );
 }
 
