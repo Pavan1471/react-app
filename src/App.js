a6f917bbb9301ed8b6aeca380517b299971adfae
Puttu Pavan Kumar <101029891+Pavan1471@users.noreply.github.com>
2023-08-02 19:05:07 +0530

    modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import './App.css';
 import { ConnectWallet } from "@thirdweb-dev/react";
 import { ThirdwebProvider } from '@thirdweb-dev/react';
 
 const App = () => (
-  <ThirdwebProvider>
+  <center>
+    <ThirdwebProvider>
       <ConnectWallet />
   </ThirdwebProvider>
+  </center>
 );
 
 export default App;
